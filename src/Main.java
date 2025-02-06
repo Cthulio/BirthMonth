8c1b9d916946cfd8c982ee93ee493748fdccef07
Nolan Crone <crone.nolan@gmail.com>
2025-02-05 20:56:34 -0500

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //class BirthMonthCalc
+        //  main()
+        //      num birthMonth
+        //      Output “What is your birth month (1-12)?”
+        //      Input birthMonth
+        //      if birthMonth < 1 or birthMonth > 12 then
+        //          Output "You entered an incorrect month value: " + birthMonth
+        //      else
+        //          Output "Your birth month is: " + birthMonth
+        //      end if
+        //  return
+        //end class
+    }
+}
