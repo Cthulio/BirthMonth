a66cb73ba6ec3ccc6a04d2c98d13398a8a3dde05
Nolan Crone <crone.nolan@gmail.com>
2025-02-08 18:41:06 -0500

    finalized code, ran checks.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -14,5 +15,18 @@ public class Main {
         //      end if
         //  return
         //end class
+        Scanner in = new Scanner(System.in);
+        int birthMonth;
+        System.out.println("What is your birth month (1-12)?");
+        birthMonth = in.nextInt();
+        in.nextLine();
+        if(birthMonth >= 1 && birthMonth <= 12) {
+                System.out.println("Your birth month is: " + birthMonth);
+
+        }
+        else
+        {
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
     }
 }
